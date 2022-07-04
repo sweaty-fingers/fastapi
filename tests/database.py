404ff9a08f5d26fa8d27dec6ab81507e96e76515
HyeonGeunY <hgyoon0928@gmail.com>
2022-07-04 10:23:36 +0900

    testing

---
+++ tests/database.py
@@ -0,0 +1,60 @@
+from fastapi.testclient import TestClient
+import pytest
+from app.main import app
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database import Base, get_db
+from app.config import settings
+from alembic import command
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+
+# Dependency
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
+
+# @pytest.fixture
+# def client():
+#     # TestClient를 반환하기 전 코드를 실행
+#     Base.metadata.drop_all(bind=engine)
+#     Base.metadata.create_all(bind=engine)
+#     #command.upgrade("head")
+#     yield TestClient(app)
+#     # command.downgrade("base")
+#     # run our code after our test finishes
+#     # Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture(scope="module")
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture(scope="module")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+# session과 client fixture를 나눔으로써 test 코드에 db를 별도로 넣어주어 테스트할 수 있게 되었다.
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,39 +1,23 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
+from .database import client, session #client가 session을 실행하므로 필요
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.database import Base, get_db
-from app.config import settings
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-
-# Dependency
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/", )
     print(res.json().get('message'))
     assert res.json().get('message') == 'Hello wwwwooorrlld change aaabbbb'
     assert res.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
+    # test코드에서는 /users뒤에 /를 붙여주지 않으면 /users/로 리디렉션이 이루어지지 않아서 에러가 발생한다.
     res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
     new_user = schemas.UserOut(**res.json()) # schemas에 필요한 요소들이 모두 포함되어 있는지 확인한다.
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(client):
+    res = client.post(
+        "/login", data={"username": "hello123@gmail.com", "password": "password123"}
+    )
+    assert res.status_code == 200
