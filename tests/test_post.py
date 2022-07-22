86983d429c389eb9b6913c6cf2fa65b339adeacc
HyeonGeunY <hgyoon0928@gmail.com>
2022-07-22 11:32:47 +0900

    add github action

--- tests/test_post.py
+++ tests/test_post.py
@@ -105,3 +105,29 @@ def test_update_post(authorized_client, test_user, test_posts):
     assert res.status_code == 200
     assert updated_post.title == data['title']
     assert updated_post.content == data['content']
+
+
+def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+    data = {
+        "title": "update_title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
+
+
+def test_unauthorized_user_update_post(client, test_user, test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_authorized_user_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {
+        "title": "update_title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/800000", json=data)
+
+    assert res.status_code == 404
