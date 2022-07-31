801e6488e9d9dea8d8e398e5233b23cd304e43c8
HyeonGeunY <hgyoon0928@gmail.com>
2022-07-31 22:45:52 +0900

    add github action add database

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,10 @@ from app.oauth2 import create_access_token
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 # app.dependency_overrides[get_db] = override_get_db
