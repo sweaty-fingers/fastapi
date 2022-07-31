674df3695e172522181bc8bc33be0712b40b1958
HyeonGeunY <hgyoon0928@gmail.com>
2022-08-01 01:13:44 +0900

    add github action for production ubuntu

--- app/main.py
+++ app/main.py
@@ -32,4 +32,4 @@ app.include_router(vote.router)
 @app.get("/") # http 매서드, path를 전해줌
 def root():
     # return {"message": "Hello"}
-    return {"message": "Hello successfully deployed from CI/CD pipeline"}
+    return {"message": "Hello successfully deployed from CI/CD pipeline to ubuntu"}
